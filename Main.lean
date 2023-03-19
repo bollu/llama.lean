469b18b611fba8745bd8ef8807a07a7352c0d693
Siddharth Bhat <siddu.druid@gmail.com>
2023-03-19 22:30:01 +0000

    add utils scaffold

--- Main.lean
+++ Main.lean
@@ -1,8 +1,11 @@
-import Cli
 import «Llama»
+import Cli
 import Lean
+import Std 
 
 open Cli
+open Std
+
 
 open Lean in
 def String.toFloat? (s: String) : Option Float :=
@@ -15,20 +18,76 @@ instance : ParseableType Float where
     | s  => s.toFloat?
 
 
-structure GptParams where 
-  nthreads : Nat
-  npredict : Nat
-  topk : Nat
-  topp : Nat
-  repeatLastN : Nat
-  repeatPenalty : Float
-  nctx : Nat
-  temp : Float
-  nbatch : Nat
+-- https://github.com/ggerganov/llama.cpp/blob/da5303c1ea68aa19db829c634f1e10d08d409680/utils.h#L15
+structure GptParams where
+  seed : Int := -1
+  nthreads : Nat := 4
+  npredict : Nat := 128
+  repeatLastN : Nat := 64
+  nCtx : Nat := 512
 
+  -- sampling parameters
+  topK : Nat := 40 
+  topP : Float := 0.95
+  temp : Float := 0.8
+  repeatPenalty : Float := 1.30
+
+  nbatch : Nat := 8 -- batch size
+
+  model : String := "models/lamma-7B/ggml-model.bin"
+  prompt : String := ""
+
+  randomPrompt : Bool := False
+  useColor : Bool := False
+  interactive : Bool := False -- interactive mode
+  interactiveStart : Bool := False -- reverse prompt immediately
+  antiprompt : List String -- string upon seeing which more user input is prompted
+  instruct : Bool := False -- instruct mode.
+  ignoreEos : Bool := False -- do not stop generating after EOS.
+
+
+structure gptvocab where 
+  token : String 
+  token2id : HashMap Int String
+  id2token : HashMap String Int 
+
+-- TODO: do I need 'int32'?
+abbrev gptvocab.id : Type := UInt32
+
+def gptvocab.gpt_tokenize (vocab: gptvocab) (text: String): Array gptvocab := sorry
+
+-- sentencepiece tokenization. 
+def gptvocab.llama_tokenize (vocab: gptvocab) (text: String) (bos: Bool): Array gptvocab := sorry 
+
+-- load the tokens from encoder.json
+def gptvocab.init (fname : String): Option gptvocab := sorry 
+
+-- random generation monad.
+abbrev LlamaM := ReaderT GptParams IO 
+
+
+-- sample next token given probabilities for each embedding
+-- - consider only the top K tokens
+-- - from them, consider only the top tokens with cumulative probability > P
+def gptvocab.llama_sample_top_p_top_k (vocab: gptvocab) (logits: Array Float)
+  (lastNTokens: Array gptvocab.id)
+  -- (repeatPenalty : Double)
+  -- (topK : Nat)
+  -- (topP : Double)
+  -- (temp : Double) 
+  : LlamaM gptvocab.id := sorry 
+ 
+-- filter to top K tokens from list of logits
+def gptvocab.sample_top_k (vocab: gptvocab) 
+  (logitsId : Array (Float × gptvocab.id))
+  -- (topK: Nat)
+  : LlamaM gptvocab.id := sorry 
+
+-- https://github.com/ggerganov/llama.cpp/blob/da5303c1ea68aa19db829c634f1e10d08d409680/utils.cpp#L18
 def runLlama (p : Parsed) : IO UInt32 := do
   return 0
 
+-- https://github.com/ggerganov/llama.cpp/blob/da5303c1ea68aa19db829c634f1e10d08d409680/utils.cpp#L18
 def llama : Cmd := `[Cli|
   llama VIA runLlama; ["0.0.1"]
   "llama language model runner (powered by L∃∀N4)."
--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 require Alloy from git "https://github.com/tydeu/lean4-alloy.git"@"334407"
 require Cli from git "https://github.com/mhuisi/lean4-cli.git"@"nightly"
-
+require Std from git "https://github.com/leanprover/std4"@"529a6"
 package «llama» {
   -- add package configuration options here
 }
