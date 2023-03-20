c48c98f4c9ee381d52b0a585e79fd9e7f812b885
Siddharth Bhat <siddu.druid@gmail.com>
2023-03-20 01:52:45 +0000

    build ggml

--- lakefile.lean
+++ lakefile.lean
@@ -16,3 +16,16 @@ lean_lib «Llama» {
 lean_exe «llama» {
   root := `Main
 }
+
+extern_lib «ggml» (pkg : Package) := do
+  -- build with cmake and make
+  let ggmlBaseDir : FilePath := pkg.dir / "ggml"
+  let ggmlBuildDir := ggmlBaseDir / "build"
+  IO.FS.createDirAll ggmlBuildDir
+  proc { cmd := "cmake", args := #["../", "-DCMAKE_POSITION_INDEPENDENT_CODE=ON"], cwd := ggmlBuildDir }
+  proc { cmd := "cmake", args := #["--build", "src/"], cwd := ggmlBuildDir }
+  -- copy library
+  let tgtPath := pkg.libDir / "libggml.a"
+  IO.FS.writeBinFile tgtPath (← IO.FS.readBinFile (ggmlBuildDir / "src" / "libggml.a"))
+  -- give library to lake
+  pure (BuildJob.pure tgtPath)
